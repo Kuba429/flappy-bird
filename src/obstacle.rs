9ad5c2ae56347a9907ad0af5d27ba28431fbb28f
Kuba429 <jakubzawadzki429@gmail.com>
2023-05-11 19:15:55 +0200

    obstacles, positions and sizes

--- src/obstacle.rs
+++ src/obstacle.rs
@@ -13,8 +13,9 @@ pub fn spawn_obstacle(mut commands: Commands, mut last_obstacle_res: ResMut<Last
         return;
     };
     let gap = 200.0;
-    let y =
-        rand::thread_rng().gen_range((-crate::WINDOW_HEIGHT / 2.0)..(crate::WINDOW_HEIGHT / 2.0));
+    let y = rand::thread_rng().gen_range(
+        (-crate::WINDOW_HEIGHT / 2.0 + gap + 10.0)..(crate::WINDOW_HEIGHT / 2.0 - gap - 10.0),
+    );
     let first_height = (crate::WINDOW_HEIGHT / 2.0) - y - (gap / 2.0);
     commands.spawn((
         SpriteBundle {
@@ -29,6 +30,43 @@ pub fn spawn_obstacle(mut commands: Commands, mut last_obstacle_res: ResMut<Last
                     x: 50.0,
                     y: first_height,
                 }),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Obstacle,
+    ));
+    let second_height = crate::WINDOW_HEIGHT - first_height - gap;
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3 {
+                x: crate::WINDOW_WIDTH / 2.0 + 50.0,
+                y: (second_height / 2.0) - (crate::WINDOW_HEIGHT / 2.0),
+                z: 0.0,
+            }),
+            sprite: Sprite {
+                color: Color::GREEN,
+                custom_size: Some(Vec2 {
+                    x: 50.0,
+                    y: second_height,
+                }),
+                ..Default::default()
+            },
+            ..Default::default()
+        },
+        Obstacle,
+    ));
+    //
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform::from_translation(Vec3 {
+                x: crate::WINDOW_WIDTH / 2.0 + 50.0,
+                y,
+                z: 0.0,
+            }),
+            sprite: Sprite {
+                color: Color::RED,
+                custom_size: Some(Vec2 { x: 10.0, y: gap }),
                 ..Default::default()
             },
             ..Default::default()
