59dde547768e754fd3fa711b07ed8fe2716a941f
Kuba429 <jakubzawadzki429@gmail.com>
2023-05-13 13:27:40 +0200

    cleaning up

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use bevy::{
     window::{WindowPlugin, WindowResolution},
     DefaultPlugins,
 };
-use obstacle::{move_obstacles, LastObstacleDistance, spawn_obstacle};
+use obstacle::{move_obstacles, spawn_obstacle, LastObstacleDistance};
 use player::{fall, handle_jump, keep_on_screen, spawn_player};
 
 pub const WINDOW_HEIGHT: f32 = 600.0;
@@ -23,17 +23,16 @@ fn main() {
             ..Default::default()
         }))
         .insert_resource(ClearColor(Color::WHITE))
-        .insert_resource(LastObstacleDistance(crate::WINDOW_WIDTH))
-        .add_startup_system(setup)
+        .add_startup_system(spawn_camera)
+        // player
         .add_startup_system(spawn_player)
-        .add_system(handle_jump)
-        .add_system(fall)
-        .add_system(keep_on_screen)
-        .add_system(move_obstacles)
-        .add_system(spawn_obstacle)
+        .add_systems((handle_jump, fall, keep_on_screen))
+        // obstacles
+        .insert_resource(LastObstacleDistance(crate::WINDOW_WIDTH))
+        .add_systems((move_obstacles, spawn_obstacle))
         .run();
 }
 
-fn setup(mut commands: Commands) {
+fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
